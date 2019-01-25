d8971107b7bc6465dd5a07ff27c02fee0f2fc49e
Alexander Safronov <me@safron.pro>
2019-01-25 15:47:26 +0300

    tsv+builtin: throwing ParseError on empty lines in the strict mode

--- src/BuiltinParser.php
+++ src/BuiltinParser.php
@@ -14,9 +14,14 @@ class BuiltinParser extends BaseParser
     {
         [$stream, $isTmp] = toStream($stream);
         try {
+            $num = 0;
             while (!feof($stream)) {
                 $row = fgetcsv($stream, 0, $this->options->separator);
+                $num++;
                 if ((count($row) === 1) && is_null($row[0])) {
+                    if ($this->options->strict) {
+                        throw new ParseException("Line {$num} is empty (empty lines are not allowed in the strict mode)");
+                    }
                     continue;
                 }
                 yield $row;
--- src/TsvParser.php
+++ src/TsvParser.php
@@ -17,13 +17,23 @@ class TsvParser extends BaseParser
         return $this->options->autoEscape ? unescaped($rows) : $rows;
     }
 
+    /**
+     * @param $stream
+     * @return \Iterator
+     * @throws ParseException
+     */
     private function generate($stream): \Iterator
     {
         [$stream, $isTmp] = toStream($stream);
         try {
+            $num = 0;
             while (!feof($stream)) {
                 $line = rtrim(fgets($stream), "\r\n");
+                $num++;
                 if (empty($line)) {
+                    if ($this->options->strict) {
+                        throw new ParseException("Line {$num} is empty (empty lines are not allowed in the strict mode)");
+                    }
                     continue;
                 }
                 yield explode($this->options->separator, $line);
